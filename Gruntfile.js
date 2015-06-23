04a39bb7bb029a714c033a05fe649d8aeeec7da2
mcoy37 <mcoy37@gmail.com>
2015-06-23 09:14:58 +0800

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,168 @@
+// Grunt tasks
+module.exports = function (grunt) {
+
+	'use strict';
+
+	// Setup folders name, so if you wnat use a different folder structure, just update this variables
+	var config = {
+		dirName: 'assets',
+		srcName: 'src'
+	}
+
+	// Unified Watch Object asign variables for easy editing
+	var watchFiles = {
+		clientJS:   [config.dirName + '/js/*.js', config.dirName + '/js/vendor/*.js'],
+		clientSrc:  [config.srcName + '/scripts/*.js'],
+		clientCSS:  [config.dirName + '/css/**/*.css'],
+		clientPreprocessor: [config.srcName + '/preprocessor/*.less', config.srcName + '/preprocessor/*.scss', config.srcName + '/preprocessor/**/*.less',],
+		clientHTML: ['/*.html'],
+		concatBase: [config.srcName + '/scripts/*js', 'src/vendor/*js' ]
+	};
+	// Define the configuration for all the tasks
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		banner: '/*!\n' +
+			'* <%= pkg.name %> - v<%= pkg.version %> - MIT LICENSE <%= grunt.template.today("yyyy-mm-dd") %>. \n' +
+			'* @author <%= pkg.author %>\n' +
+			'*/\n',
+		// Project settings
+		clean: {
+			build: {
+    			src: [ config.dirName + '/css/*.css', config.dirName + '/js/*.js']
+  			},
+		},
+		// Watches files for changes and runs tasks based on the changed files
+		watch: {
+			clientJS: {
+				files: watchFiles.clientJS,
+				tasks: ['jshint'],
+				options: {
+					livereload: true
+				}
+			},
+			clientCSS: {
+				files: watchFiles.clientCSS,
+				tasks: ['csslint'],
+				options: {
+					livereload: true
+				}
+			},
+			clientPreprocessor: {
+				files: watchFiles.clientPreprocessor,
+				tasks: ['less', 'sass'],
+				options: {
+					livereload: true
+				}
+			},
+			clientHTML: {
+				files: watchFiles.clientHTML,
+				//tasks: ['csslint'],
+				options: {
+					livereload: true
+				}
+			}
+		},
+		// Make sure code styles are up to par and there are no obvious mistakes
+		jshint: {
+			all: {
+				src: watchFiles.clientJS.concat(watchFiles.clientSrc),
+				options: {
+					jshintrc: '.jshintrc',
+					reporter: require('jshint-stylish'),
+					ignores: ['assets/js/*.min.js']
+				}
+			}
+		},
+		csslint: {
+			options: {
+				csslintrc: '.csslintrc'
+			},
+			all: {
+				src: watchFiles.clientCSS
+			}
+		},
+		concat: {
+			options: {
+				banner: '<%= banner %>',
+				stripBanners: false
+			},
+			base: {
+				src: watchFiles.concatBase,
+				dest: 'assets/js/scripts.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '<%= banner %>',
+				report: 'min'
+			},
+			base: {
+				src: ['<%= concat.base.dest %>'],
+				dest: config.dirName + '/js/scripts.min.js'
+			}
+		},
+		recess: {
+			options: {
+				compile: true,
+				banner: '<%= banner %>'
+			},
+			style: {
+				options: {
+					compress: false
+				},
+				src: [config.srcName + '/preprocessor/*.less', config.srcName + '/preprocessor/**/*.less'],
+				dest: config.dirName + '/css/styles.css'
+			}
+		},
+		sass: {
+			dist: {
+				files: [{
+					'assets/css/<%= pkg.name %>-style.css': 'src/preprocessor/*.scss'
+				}]
+			}
+		},
+		connect: {
+			server: {
+				options: {
+					keepalive: true,
+					port: 8000,
+					base: '.',
+					hostname: 'localhost',
+					debug: true,
+					livereload: true,
+					open: true
+				}
+			}
+		},
+		concurrent: {
+			tasks: ['connect', 'watch'],
+			options: {
+				logConcurrentOutput: true
+			}
+		}
+	});
+
+	// Time grunt to measure the tasks time
+	require('time-grunt')(grunt);
+
+	// Load grunt tasks automatically
+	require('load-grunt-tasks')(grunt);
+
+	// Making grunt default to force in order not to break the project if something fail.
+	grunt.option('force', true);
+
+	// Development task(s).
+	grunt.registerTask('dev', ['concurrent']);
+
+	// Css task(s).
+	grunt.registerTask('less', ['recess']);
+	grunt.registerTask('sass', ['sass']);
+
+	// Lint task(s).
+	grunt.registerTask('lint', ['jshint', 'csslint']);
+
+	// Build task(s).
+	grunt.registerTask('build', ['lint', 'concat', 'uglify']);
+
+	grunt.registerTask('default', ['concurrent']);
+};
---
+++ README.md
@@ -0,0 +1,56 @@
+Front-end Boilerplate. 
+=====================
+Reference and Credits
+https://github.com/newaeonweb/frontendboilerplate
+
+####  Disclaimer
+All code here are based on lessons learned and good practices, however many of them are fruits of research in several blog, tutorials, lectures and other sources.
+
+#### License
+[MIT]()
+
+---
+
+#### Initialize
+```
+npm install
+
+```
+
+#### Get Started
+```
+grunt
+
+```
+
+## Application Directory Layout
+We propose a folder structure for web projects from the scratch, because all web projects have CSS, JS and Images folders.
+
+```
+	assets/					--> all of the files to be used in production
+		css					--> css files
+		fonts				--> Font-face folder
+		images				--> image files
+		js/					--> javascript files
+			vendor 			--> third party libraries copied from src/vendor
+	lib/					--> 3rd party bower libraries
+	src/					--> scripts for development
+		scripts/			--> hand made JavaScript files, plugins and others
+		preprocessor/		--> your choice for Sass or Less
+		vendor/				--> third party libraries like: respond, mçodernizer
+	test					--> test source files and libraries
+		mocha				--> mocha folder structure for visual test results
+			css/			
+				mocha.css
+			js/				--> testing frameworks file
+				chai.js
+				mocha.js
+		spec				--> specs for testing
+			app-test.js		--> example on how to write tests
+		test.html			--> visual page for tests
+	index.html				--> base layout file
+	Gruntfile.js			--> where the magic happens
+```
+
+
+
